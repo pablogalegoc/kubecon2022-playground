20fe0a5ab26f15953d907488e02ec1e952ff151a
Pablo Galego <pgalego@vmware.com>
2022-05-08 17:55:04 +0200

    Prepare vulnerability db and folders for a deterministic setup

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+tar -C ./trivy-cache -xzf ./utils/trivy-db-36ece5d0500a9e8ab56053bb702ca67a5f9956234a5a5b5325d7f4f824dbbd2e.tar.gz
+export EMAIL=myemail@kube.con AUTH=ups-this-is-a-secret
