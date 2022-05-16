151a90adc760f2d74726b89e2c10332acf22da71
Pablo Galego <pgalego@vmware.com>
2022-05-16 23:10:31 +0200

    Fix typo in get-plugins script

--- demo-service/src/main/dist/get-plugins.sh
+++ demo-service/src/main/dist/get-plugins.sh
@@ -5,4 +5,4 @@ PLUGINS_TARGET_DIR=${PLUGINS_TARGET_DIR:-/plugins}
 
 mkdir -p "$PLUGINS_TARGET_DIR"
 # Cheap way of substituting a Maven command to get a JAR from a repository
-curl -L https://github.com/pablogalegoc/kubecon2022-playground/releases/download/release%2Fhello-plugin%2F0.1.0/hello-plugin-plugin-0.1.0-with-dependencies.jar --output "${PLUGINS_TARGET_DIR}/hello-plugin-plugin-0.1.0-with-dependencies.jar"
+curl -L https://github.com/pablogalegoc/kubecon2022-playground/releases/download/release%2Fhello-plugin%2F0.1.0/hello-plugin-plugin-0.1.0-with-dependencies.jar --output "${PLUGINS_TARGET_DIR}/hello-plugin-0.1.0-with-dependencies.jar"
